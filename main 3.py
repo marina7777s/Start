de90a1e2ba82d41b41761b2019403e8ef7306f54
marina7777s <belousow4.m@yandex.ru>
2024-08-02 17:31:29 +0300

    Практическое задание по работе с "Переменными"

---
+++ main 3.py
@@ -0,0 +1,5 @@
+number_of_tasks = 12
+time = 1.5
+name_course = "Python"
+result = time / number_of_tasks
+print("Курс:",name_course,", всего задач:",number_of_tasks,", затрачено часов:",time,", среднее время выполнения", result,"часа")
