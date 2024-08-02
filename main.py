5a3faba065116d5f36962ae648520fad381f03b6
marina7777s <belousow4.m@yandex.ru>
2024-08-02 17:57:45 +0300

    Практическое задание "Базовые структуры данных"

--- main.py
+++ main.py
@@ -5,18 +5,5 @@ print((2 + 3 + 4) // 3)
 print("Понедельник,  Вторник")
 print((((2 * 3) + (2 * 4)) ** 3) / 2)
 
-# "Ist program"
-print((9 ** 0.5) * 5)
-# "2nd program"
-print(9.99 > 9.98 and 1000 != 1000.1)
-# "3rd program"
-num = 1234
-mid = str(num) [1 : 3]
-print(int(mid))
-tip = 5678
-sok = str(tip) [1 : 3]
-print(int(sok))
-print(((1234 % 1000) // 10) + ((5678 % 1000) // 10))
-# "4tr program"
-print(((int((13.42 % 100) // 1)) == (int(round(42.13 % 1, 2) * 100))) or ((int((42.13 % 100) // 1)) == (int(round(13.42 % 1, 2) * 100))))
+
 
