ec30adb486e0e6f86bb68b6a2f0cd7fe39cb8847
marina7777s <belousow4.m@yandex.ru>
2024-08-02 20:41:14 +0300

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -3,6 +3,6 @@ print("Имя:",name)
 age = 44
 print("Возраст:",age)
 new_age = 45
-print("Новый возвраст:",new_age)
+print("Новый возраст:",new_age)
 is_student = "student" == "student"
 print("Студент:",is_student)
