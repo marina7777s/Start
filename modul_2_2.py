726effebd8b67da67a7e460b1df007a906e56a76
marina7777s <belousow4.m@yandex.ru>
2024-08-07 11:01:37 +0300

    "Условная конструкция. Операторы if, elif, else"

---
+++ modul_2_2.py
@@ -0,0 +1,13 @@
+first = (123)
+second = (456)
+third = (789)
+if first == second and second == third and first == third:
+    print(3)
+else:
+    print(0)
+first = 42
+second = 69
+third = 42
+if first == second or second == third or first == third:
+    print(2)
+
